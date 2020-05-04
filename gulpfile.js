b8a819a49fd2cede9abbc1822a3049a06bd29598
makowskib <58569878+makowskib@users.noreply.github.com>
2020-05-03 21:16:34 -0400

    added lintjs to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,3 +19,14 @@ let validateHTML = () => {
 };
 
 exports.validateHTML = validateHTML;
+
+const { src } = require(`gulp`);
+const jsLinter = require(`gulp-eslint`);
+
+let lintJS = () => {
+    return src(`scripts/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
+exports.lintJS = lintJS;
