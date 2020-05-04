79787e29126f05a16952c80963a2fc434e597429
makowskib <58569878+makowskib@users.noreply.github.com>
2020-05-03 21:15:33 -0400

    added validatehtml to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,3 +10,12 @@ let compressHTML = () => {
 
 exports.compressHTML = compressHTML;
 
+const { src } = require(`gulp`);
+const htmlValidator = require(`gulp-html`);
+
+let validateHTML = () => {
+    return src(`html-files/*.html`)
+        .pipe(htmlValidator());
+};
+
+exports.validateHTML = validateHTML;
